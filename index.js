f9ad4cc5ebc29fbe8a7d3389041bb07a7430f21b
zdog32 <67707704+zdog32@users.noreply.github.com>
2020-07-01 14:14:58 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function count(){
+    number = number + 1;
+    displayNumber.innerText = number;
+}
+function uncount(){
+    number = number - 1;
+    displayNumber.innerText = number;
+}
+function square(){
+    number = number * number;
+    displayNumber.innerText = number;
+}
+function reset(){
+    number = 0;
+    displayNumber.innerText = number;
+}
+var displayNumber = document.querySelector("#displayNumber");
+var number = 0;

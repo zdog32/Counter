c203eee32acfc9ba9c6b5c9248c525c80d0fc883
Matthew Zhou <matt.thew.zhou@gmail.com>
2020-07-06 19:00:57 -0400

    Remember Font And Number

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 function count(){
     number = number + 1;
-    displayNumber.innerText = number;
+    updateNumber();
 }
 function uncount(){
     number = number - 1;
-    displayNumber.innerText = number;
+    updateNumber();
 }
 function square(){
     number = number * number;
-    displayNumber.innerText = number;
+    updateNumber();
 }
 function reset(){
     number = 0;
-    displayNumber.innerText = number;
+    updateNumber();
 }
 function half(){
     number = number / 2;
-    displayNumber.innerText = number;
+    updateNumber();
 }
+function updateNumber(){
+    code.innerHTML = number;
+    document.cookie = "number="+number;
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
 var displayNumber = document.querySelector("#displayNumber");
-var number = 0;
+var number = Number(getCookie("number"));
+if (!number || isNaN(number)){
+    number = 0;
+}
+var code = document.querySelector("#number");
+code.innerHTML = number;
+
